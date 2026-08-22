05a3b5f3efc03213962887d5cdf4b0b93d367bf7
floppa <gammy1325@gmail.com>
2026-08-22 01:34:32 -0500

    Add line breaks to employee information output

--- informacion.php
+++ informacion.php
@@ -4,8 +4,8 @@ $NomApellidos = $_POST['NomApellidos'];
 $Cargo = $_POST['Cargo'];
 $Salario = $_POST['Salario'];
 
-echo "La identificacion del Empleado es: " . $Identificacion;
-echo "Los nombres y apellidos del empleado son: " . $NomApellidos;
-echo "El cargo del empleado es: " . $Cargo;
-echo "El salario del empleado es: " . $Salario;
+echo "La identificacion del Empleado es: " . $Identificacion . "<br>";
+echo "Los nombres y apellidos del empleado son: " . $NomApellidos . "<br>";
+echo "El cargo del empleado es: " . $Cargo . "<br>";
+echo "El salario del empleado es: " . $Salario . "<br>";
 ?>
